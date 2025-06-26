71c0c4437bfea6568472ab479ed1d5997920c206
Tynberry <28360155+tynberry@users.noreply.github.com>
2025-06-26 17:04:58 +0200

    feat: add some kind of a prelude

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,12 @@ mod c_types;
 pub mod component;
 pub mod entity;
 pub mod flecs;
+pub mod prelude;
 pub mod query;
 pub mod system;
 #[cfg(test)]
 mod test;
 pub mod world;
 
-//publically exposes raw bindings
+//publically expose raw bindings
 pub use flecs_ecs_sys as sys;
---
+++ src/prelude.rs
@@ -0,0 +1,15 @@
+//List of useful imports.
+
+pub use crate::component::Component;
+pub use crate::component::ComponentView;
+pub use crate::component::id::id;
+pub use crate::entity::Entity;
+pub use crate::entity::EntityView;
+pub use crate::query::Query;
+pub use crate::query::QueryBuilder;
+pub use crate::query::iter::Iter;
+pub use crate::system::SystemBuilder;
+pub use crate::world::World;
+
+pub use flecs_ecs_sys as sys;
+
